d3434090b7cc4c9655ac3a16ff341f942b7af141
Kofu145g <kofu145g@gmail.com>
2021-09-11 14:23:13 -0700

    Networking project setup
    Set up testing application and networking library, under the same solution

---
+++ OshiNetTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OshiNetTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
