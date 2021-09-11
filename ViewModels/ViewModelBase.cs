42bfc79270bfb560fa78f1310727ca27c25905ab
Kofu145g <kofu145g@gmail.com>
2021-09-10 23:26:35 -0700

    Avalonia Project Creation
    Created the project through Visual Studio, initial commit

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oshi.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
